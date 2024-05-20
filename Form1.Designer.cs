13e57f23c1cb5369130a93d9a2c1dcc4df58c308
kirb <kirb@NGHIATRAN5615>
2024-05-20 17:28:29 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace RockBoxPlaylistEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 11);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Eject";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(799, 377);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Path";
+            this.Column1.Name = "Column1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(634, 426);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Apply ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(715, 426);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Quit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 425);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Add";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(69, 425);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Remove";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Select your m3u8 file";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 454);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Rockbox Playlist Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockBoxPlaylistEditor
+{
+
+    public partial class Form1 : Form
+    {
+
+        string path;
+        string path2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr CreateFile(
+ string lpFileName,
+ uint dwDesiredAccess,
+ uint dwShareMode,
+ IntPtr SecurityAttributes,
+ uint dwCreationDisposition,
+ uint dwFlagsAndAttributes,
+ IntPtr hTemplateFile
+);
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool DeviceIoControl(
+            IntPtr hDevice,
+            uint dwIoControlCode,
+            IntPtr lpInBuffer,
+            uint nInBufferSize,
+            IntPtr lpOutBuffer,
+            uint nOutBufferSize,
+            out uint lpBytesReturned,
+            IntPtr lpOverlapped
+        );
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool DeviceIoControl(
+            IntPtr hDevice,
+            uint dwIoControlCode,
+            byte[] lpInBuffer,
+            uint nInBufferSize,
+            IntPtr lpOutBuffer,
+            uint nOutBufferSize,
+            out uint lpBytesReturned,
+            IntPtr lpOverlapped
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        private IntPtr handle = IntPtr.Zero;
+
+        const uint GENERIC_READ = 0x80000000;
+        const uint GENERIC_WRITE = 0x40000000;
+        const int FILE_SHARE_READ = 0x1;
+        const int FILE_SHARE_WRITE = 0x2;
+        const int FSCTL_LOCK_VOLUME = 0x00090018;
+        const int FSCTL_DISMOUNT_VOLUME = 0x00090020;
+        const int IOCTL_STORAGE_EJECT_MEDIA = 0x2D4808;
+        const int IOCTL_STORAGE_MEDIA_REMOVAL = 0x002D4804;
+
+        /// <summary>
+        /// Constructor for the USBEject class
+        /// </summary>
+        /// <param name="driveLetter">This should be the drive letter. Format: F:/, C:/..</param>
+
+        public IntPtr USBEject(string driveLetter)
+        {
+            string filename = @"\\.\" + driveLetter[0] + ":";
+            return CreateFile(filename, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, IntPtr.Zero, 0x3, 0, IntPtr.Zero);
+        }
+
+        public bool Eject(IntPtr handle)
+        {
+            bool result = false;
+
+            if (LockVolume(handle) && DismountVolume(handle))
+            {
+                PreventRemovalOfVolume(handle, false);
+                result = AutoEjectVolume(handle);
+            }
+            CloseHandle(handle);
+            return result;
+        }
+
+        private bool LockVolume(IntPtr handle)
+        {
+            uint byteReturned;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (DeviceIoControl(handle, FSCTL_LOCK_VOLUME, IntPtr.Zero, 0, IntPtr.Zero, 0, out byteReturned, IntPtr.Zero))
+                {
+                    System.Windows.Forms.MessageBox.Show("Eject completed!");
+                    return true;
+                }
+                Thread.Sleep(500);
+            }
+            return false;
+        }
+
+        private bool PreventRemovalOfVolume(IntPtr handle, bool prevent)
+        {
+            byte[] buf = new byte[1];
+            uint retVal;
+
+            buf[0] = (prevent) ? (byte)1 : (byte)0;
+            return DeviceIoControl(handle, IOCTL_STORAGE_MEDIA_REMOVAL, buf, 1, IntPtr.Zero, 0, out retVal, IntPtr.Zero);
+        }
+
+        private bool DismountVolume(IntPtr handle)
+        {
+            uint byteReturned;
+            return DeviceIoControl(handle, FSCTL_DISMOUNT_VOLUME, IntPtr.Zero, 0, IntPtr.Zero, 0, out byteReturned, IntPtr.Zero);
+        }
+
+        private bool AutoEjectVolume(IntPtr handle)
+        {
+            uint byteReturned;
+            return DeviceIoControl(handle, IOCTL_STORAGE_EJECT_MEDIA, IntPtr.Zero, 0, IntPtr.Zero, 0, out byteReturned, IntPtr.Zero);
+        }
+
+        private bool CloseVolume(IntPtr handle)
+        {
+            return CloseHandle(handle);
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                //Environment.Exit(0);
+                e.Cancel = false;
+            }
+            else
+            {
+                // your Code for Changes or anything you want to allow user changes etc.
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                string meow = string.Join(string.Empty, file.Skip(2)).Replace("\\", "/");
+                this.dataGridView1.Rows.Add(meow);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "playlist files (*.m3u8)|*.m3u8";
+            dialog.Title = "Load m3u8 playlist";
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.dataGridView1.Rows.Clear();
+
+
+                textBox1.Text = dialog.FileName;
+
+                path2 = dialog.FileName;
+
+                var fileStream = dialog.OpenFile();
+
+
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    path = reader.ReadToEnd();
+                    Console.WriteLine(path);
+
+                    using (StringReader reader2 = new StringReader(path))
+                    {
+                        string line;
+                        while ((line = reader2.ReadLine()) != null)
+                        {
+                            this.dataGridView1.Rows.Add(line);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Eject(USBEject(textBox1.Text.Substring(0, 2)));
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog2 = new OpenFileDialog();
+            dialog2.Filter = "All files (*.*)|*.*";
+            dialog2.Title = "Load songs for playlist";
+            dialog2.Multiselect = true;
+
+            if (dialog2.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = dialog2.OpenFile();
+
+                string[] allFiles = dialog2.FileNames;
+
+                foreach (string file in allFiles)
+                {
+                    string meow = string.Join(string.Empty, file.Skip(2)).Replace("\\", "/");
+                    this.dataGridView1.Rows.Add(meow);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Console.WriteLine(this.dataGridView1.Rows.Count);
+
+            try
+            {
+                using (TextWriter tw = new StreamWriter(path2))
+                {
+                    for (int i = 0; i < this.dataGridView1.Rows.Count; i++)
+                    {
+                        tw.WriteLine($"{dataGridView1.Rows[i].Cells[0].Value.ToString()}");
+                    }
+                }
+
+                MessageBox.Show("Done!", "Data written successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            } catch (Exception asdf) {
+                MessageBox.Show(asdf.Message, "Something went wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DeleteSelectedRows();
+        }
+
+        private void DeleteSelectedRows()
+        {
+            HashSet<int> rowsToDelete = new HashSet<int>();
+
+            // Collect all row indices that contain selected cells
+            foreach (DataGridViewCell selectedCell in dataGridView1.SelectedCells)
+            {
+                if (selectedCell.Selected)
+                {
+                    rowsToDelete.Add(selectedCell.RowIndex);
+                }
+            }
+
+            // Remove rows starting from the highest index to avoid index shifting issues
+            List<int> sortedRows = new List<int>(rowsToDelete);
+            sortedRows.Sort((a, b) => b.CompareTo(a)); // Sort in descending order
+
+            foreach (int rowIndex in sortedRows)
+            {
+                dataGridView1.Rows.RemoveAt(rowIndex);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
